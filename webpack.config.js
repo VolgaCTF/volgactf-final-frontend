fca907026b67bcdf79eac49ac3306c09d2b368db
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2020-09-13 16:29:11 +0000

    Add cleanup and compression plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
+const zopfli = require('@gfx/zopfli')
+const BrotliPlugin = require('brotli-webpack-plugin')
 
 function getBrandingRootPath () {
   let rootPath = process.env.BRANDING_ROOT_PATH || 'branding-default'
@@ -14,65 +17,88 @@ function getBrandingScriptPath () {
   return path.join(getBrandingRootPath(), 'js')
 }
 
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ["@babel/env"] }
+module.exports = (env, argv) => {
+  const plugins = [
+    new CleanWebpackPlugin()
+  ]
+  if (argv.mode === 'production') {
+    plugins.push(new CompressionPlugin({
+      compressionOptions: {
+        numiterations: 15
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/'
-            }
-          }
-        ]
+      algorithm(input, compressionOptions, callback) {
+        return zopfli.gzip(input, compressionOptions, callback)
       }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-    alias: {
-      Branding: getBrandingScriptPath()
-    }
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public/'),
-    port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true,
-    historyApiFallback: true
-  },
-  // plugins: [new webpack.HotModuleReplacementPlugin()]
-  plugins: [new CompressionPlugin()]
+    }))
+    plugins.push(new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }))
+  } else if (argv.mode === 'development') {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+
+  return {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          options: { presets: ["@babel/env"] }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+      alias: {
+        Branding: getBrandingScriptPath()
+      }
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+      publicPath: '/dist/',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public/'),
+      port: 3000,
+      publicPath: 'http://localhost:3000/dist/',
+      hotOnly: true,
+      historyApiFallback: true
+    },
+    plugins: plugins
+  }
 }
